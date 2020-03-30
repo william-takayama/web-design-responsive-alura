c5dd5413a708bf51ee8d81541e1ee3ccdf58a13b
William <william-sobreira@hotmail.com>
2020-03-30 14:34:44 -0300

    web responsivo

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,13 @@
-document.querySelector('.menu-controle').onclick = function() {
-	document.documentElement.classList.toggle('menu-ativo');
+document.querySelector('.menu-abrir').onclick = function() {
+    document.documentElement.classList.add('menu-ativo');
+};
+
+document.querySelector('.menu-fechar').onclick = function() {
+    document.documentElement.classList.remove('menu-ativo');
+};
+
+document.documentElement.onclick = function(event) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('menu-ativo');
+    }
 };
